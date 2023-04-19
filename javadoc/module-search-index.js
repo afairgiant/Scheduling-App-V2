377363530745db8dd1ebd1a58728791fc7c2c1f8
alex.j.fair@gmail.com <github2k!>
2023-04-19 17:13:18 -0600

    JavaDoc

--- javadoc/module-search-index.js
+++
@@ -1,1 +0,0 @@
-moduleSearchIndex = [{"l":"scheduling_app.scheduling_app"}];updateSearchResults();
--- javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"scheduling_app.scheduling_app","l":"controller"},{"m":"scheduling_app.scheduling_app","l":"helper"},{"m":"scheduling_app.scheduling_app","l":"model"},{"m":"scheduling_app.scheduling_app","l":"scheduling_app.scheduling_app"}];updateSearchResults();
